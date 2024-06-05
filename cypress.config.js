f3c47e8914210ee83de96217a4a085005c38f39b
Mumtaz <Umair.Mumtaz@snb.ca>
2024-06-05 15:23:59 -0400

    added Mochawesome reporter

--- cypress.config.js
+++ cypress.config.js
@@ -3,9 +3,11 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   chromeWebSecurity: false,
   experimentalModifyObstructiveThirdPartyCode: true,
+  reporters: 'mochawesome',
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      require("cypress-mochawesome-reporter/plugin")(on);
     },
   },
 });
